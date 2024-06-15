52fdb30ed40fe836e66c2e7405516329163e0cd3
fuyjcnbr <paall@rambler.ru>
2024-06-15 13:15:08 +0300

    coding style fix LIT002

--- test_main.py
+++ test_main.py
@@ -17,7 +17,7 @@ def test_predict_positive():
     )
     json_data = response.json()
     assert response.status_code == 200
-    assert json_data['label'] == 'POSITIVE'
+    assert json_data["label"] == "POSITIVE"
 
 
 def test_predict_negative():
@@ -27,4 +27,4 @@ def test_predict_negative():
     )
     json_data = response.json()
     assert response.status_code == 200
-    assert json_data['label'] == 'NEGATIVE'
+    assert json_data["label"] == "NEGATIVE"
